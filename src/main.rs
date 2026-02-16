2cb9ab275408cb69f3068f71a41f80e5372c4e7c
DavidLokison <contact@davidlokison.com>
2026-02-16 01:34:54 +0100

    feat: add `events/open` endpoint
    closes: #2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use rocket::{get, launch, routes};
 use rocket::http::Status;
 use serde::Serialize;
+use sqlx::FromRow;
+use time::Date;
 
 mod core;
 use crate::core::{Connection, Response};
@@ -8,6 +10,16 @@ use crate::core::{Connection, Response};
 #[derive(Serialize, Debug)]
 struct UpstreamHealth {
     ping: f32,
+}
+
+#[derive(Serialize, FromRow, Debug)]
+struct Event {
+    #[sqlx(rename = "event_id")]
+    id: String,
+    title: String,
+    begin: Date,
+    end: Date,
+    description: String,
 }
 
 #[get("/health")]
@@ -26,8 +38,18 @@ async fn check_health(mut db: Connection) -> Response<UpstreamHealth> {
     }
 }
 
+#[get("/events/open")]
+async fn get_open_events(mut db: Connection) -> Response<Vec<Event>> {
+    let events: Vec<Event> = sqlx::query_as("SELECT event_id, title, begin, end, description FROM api_events WHERE deadline >= CURRENT_DATE")
+        .fetch_all(&mut **db).await?;
+    Ok((Status::Ok, events).into())
+}
+
 #[launch]
 fn rocket() -> _ {
     core::build()
-        .mount("/", routes![check_health])
+        .mount("/", routes![
+            check_health,
+            get_open_events,
+        ])
 }
