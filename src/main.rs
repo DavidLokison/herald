a85cca8827ca85389a28932486d71d3b4d360d52
DavidLokison <contact@davidlokison.com>
2026-05-06 10:51:09 +0200

    refactor: clean up response structure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use rocket::{get, post, launch, routes};
-use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket_db_pools::Connection;
 use uuid::Uuid;
 
 mod core;
-use core::{Herald, Result, wrap};
+use core::{Herald, Result, Created, Custom};
 
 use herald::data;
 use herald::types::*;
@@ -17,27 +16,24 @@ expose_endpoint!(#[get("/events/open")] get_open_events);
 expose_endpoint!(#[get("/events/types")] get_event_types);
 
 #[get("/events/types/<event_type_slug>/items")]
-async fn get_bookable_items(mut db: Connection<Herald>, event_type_slug: &str) -> Result<Vec<Article>> {
+async fn get_bookable_items(mut db: Connection<Herald>, event_type_slug: &str) -> Result<Custom<Vec<Article>>> {
     let event_type_slug = EventTypeSlug::try_query(&mut **db, event_type_slug).await?;
     let articles = herald::data::get_bookable_items(&mut db, &event_type_slug).await?;
-    Ok(articles.into())
+    Custom::ok(articles)
 }
 
 #[get("/events/<event_id>/registrations/preview?<persons..>")]
-async fn get_registration_preview(mut db: Connection<Herald>, event_id: Uuid, persons: Vec<PriceCheck>) -> Result<impl serde::Serialize> {
+async fn get_registration_preview(mut db: Connection<Herald>, event_id: Uuid, persons: Vec<PriceCheck>) -> Result<Custom<Vec<Article>>> {
     let event_id = EventId::try_query(&mut **db, &event_id).await?;
-    herald::data::get_registration_preview(&mut db, &event_id, &persons).await
-        .map(Into::into)
-        .map_err(Into::into)
+    let preview = herald::data::get_registration_preview(&mut db, &event_id, &persons).await?;
+    Custom::ok(preview)
 }
 
 #[post("/events/<event_id>/registrations", data = "<registration>")]
-async fn create_registration(mut db: Connection<Herald>, event_id: Uuid, registration: Json<NewRegistration<'_>>) -> Result<()> {
+async fn create_registration(mut db: Connection<Herald>, event_id: Uuid, registration: Json<NewRegistration<'_>>) -> Result<Created<()>> {
     let event_id = EventId::try_query(&mut **db, &event_id).await?;
-    data::create_registration(&mut db, &event_id, &registration).await
-        .map(wrap(Status::Created))
-        .map(Into::into)
-        .map_err(Into::into)
+    let _ = data::create_registration(&mut db, &event_id, &registration).await?;
+    Created::ok("", ()) // TODO
 }
 
 #[launch]
