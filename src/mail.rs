cba8113071e7acfb3df63f739edfc7721175bb2a
DavidLokison <contact@davidlokison.com>
2026-08-01 20:54:47 +0200

    refactor: let mail library only render and not care about transport

--- src/mail.rs
+++ src/mail.rs
@@ -1,9 +1,7 @@
 use thiserror::Error;
 use serde::Serialize;
 use lettre::Message;
-use lettre::message::Mailbox;
 use lettre::message::header::ContentType;
-use lettre::transport::AsyncTransport;
 use crate::template::Template;
 
 #[derive(Error, Debug)]
@@ -16,30 +14,20 @@ pub enum MailError {
     AddressError(#[from] lettre::address::AddressError),
     #[error("message builder error")]
     BuilderError(#[from] lettre::error::Error),
-    #[error("smtp server error")]
-    SmtpError(#[from] lettre::transport::smtp::Error),
     #[error("could not find mail templates for '{0}'")]
     TemplateMissingError(String),
-    #[cfg(test)]
-    #[error("stub server error")]
-    StubError(#[from] lettre::transport::stub::Error),
 }
 
-pub async fn send_mail<T>(t: T, template: Template<'_>, contract: impl Serialize) -> Result<(), MailError>
-where
-    T: AsyncTransport + Sync,
-    MailError: From<<T as AsyncTransport>::Error>,
-{
-    let handlebars = template.mail.ok_or_else(|| MailError::TemplateMissingError(template.slug.clone()))?;
+pub fn render(template: &Template<'_>, contract: impl Serialize) -> Result<Message, MailError> {
+    let handlebars = template.mail.as_ref().ok_or_else(|| MailError::TemplateMissingError(template.slug.clone()))?;
     let mut email = Message::builder().header(ContentType::TEXT_PLAIN);
-    email = email.from(handlebars.render("sender", &contract)?.parse::<Mailbox>()?);
-    email = email.to(handlebars.render("recipient", &contract)?.parse::<Mailbox>()?);
+    email = email.from(handlebars.render("sender", &contract)?.parse()?);
+    email = email.to(handlebars.render("recipient", &contract)?.parse()?);
     if handlebars.has_template("reply_to") {
-        email = email.reply_to(handlebars.render("reply_to", &contract)?.parse::<Mailbox>()?);
+        email = email.reply_to(handlebars.render("reply_to", &contract)?.parse()?);
     }
     email = email.subject(handlebars.render("subject", &contract)?);
-    let email = email.body(handlebars.render("body", &contract)?)?;
-    t.send(email).await.map(|_r| ()).map_err(MailError::from)
+    Ok(email.body(handlebars.render("body", &contract)?)?)
 }
 
 #[cfg(test)]
@@ -47,13 +35,15 @@ mod tests {
     use std::assert_matches;
     use std::env;
     use std::pin::pin;
+    use std::future::Future;
     use std::task::{
         Poll,
         Context,
         Waker,
     };
     use crate::template::Template;
-    use super::send_mail;
+    use super::render;
+    use lettre::transport::AsyncTransport;
     use lettre::transport::stub::AsyncStubTransport;
 
     #[test]
@@ -61,7 +51,8 @@ mod tests {
         env::set_current_dir(format!("{}/tests", env!("CARGO_MANIFEST_DIR"))).unwrap();
         let template = Template::load("healthcheck").unwrap();
         let transport = AsyncStubTransport::new_ok();
-        let future = send_mail(transport, template, {});
+        let email = render(&template, {}).unwrap();
+        let future = transport.send(email);
         let mut future = pin!(future);
         let mut context = Context::from_waker(Waker::noop());
         let mut poll = Poll::Pending;
@@ -69,6 +60,6 @@ mod tests {
             poll = future.as_mut().poll(&mut context);
         }
         let Poll::Ready(result) = poll else { unreachable!() };
-        assert_matches!(result, Ok(()));
+        assert_matches!(result, Ok(_));
     }
 }
